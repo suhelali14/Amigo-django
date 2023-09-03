90a5d2553b4847e8741ae5ef755b55bdbc60ec32
suhelali14 <106341120+suhelali14@users.noreply.github.com>
2023-09-04 01:22:34 +0530

    Delete Amigoo directory

--- Amigoo/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Post, LikePost, FollowersCount,Usermode
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(LikePost)
-admin.site.register(FollowersCount)
-admin.site.register(Usermode)
